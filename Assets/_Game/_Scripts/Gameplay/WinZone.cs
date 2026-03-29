e16342d100fe2118d6e9bec90ccdf60df1d47c7e
umuterkus <umuterkusofficial@gmail.com>
2026-03-29 16:24:33 +0300

    feat: new levels added

--- Assets/_Game/_Scripts/Gameplay/WinZone.cs
+++ Assets/_Game/_Scripts/Gameplay/WinZone.cs
@@ -1,15 +1,28 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class WinZone : MonoBehaviour
 {
+    private Coroutine _winCoroutine;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Item"))
-        {
-            if (GameManager.Instance.CurrentState != GameManager.GameState.Playing) return;
-            GameManager.Instance.ChangeState(GameManager.GameState.Win);
-        }
+            _winCoroutine = StartCoroutine(WinRoutine());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Item") && _winCoroutine != null)
+            StopCoroutine(_winCoroutine);
+    }
+
+    private IEnumerator WinRoutine()
+    {
+        yield return new WaitForSeconds(0.6f);
+        if (GameManager.Instance.CurrentState != GameManager.GameState.Playing) yield break;
+        GameManager.Instance.ChangeState(GameManager.GameState.Win);
     }
 
 }
